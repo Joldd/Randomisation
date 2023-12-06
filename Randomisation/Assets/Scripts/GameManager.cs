f654f4997c317977e30416982fb554710dabac4d
Corentin <ccouasno@gmail.com>
2023-12-06 10:49:54 +0100

    Chests can be opened in order

--- Randomisation/Assets/Scripts/GameManager.cs
+++ Randomisation/Assets/Scripts/GameManager.cs
@@ -1,30 +1,43 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Utils.ExtensionMethods;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] int _seed = -1;
+    [SerializeField] List<Chest> _chests;
 
     [ContextMenu("Play")]
     public void Play()
     {
-        var seed = Init(_seed == -1 ? null : _seed);
+        var seed = Init(_seed);
 
         Debug.Log(seed);
         
-        var chests = Enumerable.Range(1, 10).OrderBy(_ => Random.value);
+        var chestIds = Enumerable.Range(0, 10).OrderBy(_ => Random.value).ToList();
 
-        Debug.Log(chests.ToDisplayString());
+        Debug.Log(chestIds.ToDisplayString());
+
+        for (var i = chestIds.Count - 1; i >= 0; i--)
+        {
+            var chest = _chests.Single(chest => chest.Id == chestIds[i]);
+            Chest prevChest = null;
+            
+            if (i >= 1)
+                prevChest = _chests.Single(chest => chest.Id == chestIds[i - 1]);
+
+            chest.ChestToOpen = prevChest;
+        }
     }
 
-    int Init(int? seed = null)
+    int Init(int seed = -1)
     {
         // Generates a random seed if not given
-        if (seed == null)
+        if (seed == -1)
             seed = System.Environment.TickCount;
         
-        Random.InitState(seed.Value);
-        return seed.Value;
+        Random.InitState(seed);
+        return seed;
     }
 }
