5085c05c023419f93c218377ebcf01cc7772d757
Joldd <julien.coutouly@gmail.com>
2023-12-06 10:21:22 +0100

    click to open

--- Randomisation/Assets/Scripts/chest.cs
+++ Randomisation/Assets/Scripts/chest.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class chest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("salut");
-    }
+    [SerializeField]
+    Animator animator;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        animator.Play("open");
     }
 }
