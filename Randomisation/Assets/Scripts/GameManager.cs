7b7001b9ca4ba35e8c902618b230ab9a822f552e
Corentin <ccouasno@gmail.com>
2023-12-06 10:05:45 +0100

    Arrange an array randomly

---
+++ Randomisation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using UnityEngine;
+using Utils.ExtensionMethods;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] int _seed = -1;
+
+    [ContextMenu("Play")]
+    public void Play()
+    {
+        var seed = Init(_seed == -1 ? null : _seed);
+
+        Debug.Log(seed);
+        
+        var chests = Enumerable.Range(1, 10).OrderBy(_ => Random.value);
+
+        Debug.Log(chests.ToDisplayString());
+    }
+
+    int Init(int? seed = null)
+    {
+        // Generates a random seed if not given
+        if (seed == null)
+            seed = System.Environment.TickCount;
+        
+        Random.InitState(seed.Value);
+        return seed.Value;
+    }
+}
---
+++ Randomisation/Assets/Scripts/Utils/ExtensionMethods/CollectionsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Utils.ExtensionMethods
+{
+    public static class CollectionsExtensions
+    {
+        public static string ToDisplayString<T>(this IEnumerable<T> source)
+        {
+            if (source == null)
+                return "null";
+
+            string s = "[";
+            s = source.Aggregate(s, (res, x) => res + x + ", ");
+
+            if (s.Contains(", "))
+                s = s.Substring(0, s.Length - 2);
+
+            return $"{s}]";
+        }
+
+    }
+}
